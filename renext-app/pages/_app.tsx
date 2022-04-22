dcd12e456c07850ffe650c1e85969fede06ecdf4
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-04-22 21:25:13 +0600

    feat: add basic css property

--- renext-app/pages/_app.tsx
+++ renext-app/pages/_app.tsx
@@ -1,8 +1,23 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>DrTop - best shmule</title>
+        <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
