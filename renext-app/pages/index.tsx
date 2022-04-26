455b012d614a65e696a10e6bbd85ebda8c483ca3
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-04-26 08:38:16 +0600

    feat: add paragraph componenet

--- renext-app/pages/index.tsx
+++ renext-app/pages/index.tsx
@@ -1,11 +1,14 @@
-import { Button, Htag } from '../components'
+import { Button, Htag, P } from '../components'
 
 export default function Home(): JSX.Element {
   return (
 	<>
 		<Htag tag='h1' >Текст</Htag>
 		<Button appearance='primary' arrow='right' >Button</Button>
-		<Button appearance='ghost' arrow='right'  >Button</Button>
+		<Button appearance='ghost' arrow='down'  >Button</Button>
+		<P size='l'>Large</P>
+		<P size='m'>Medium</P>
+		<P size='s'>Small</P>
 	</>
   )
 }
