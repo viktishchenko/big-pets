3d11b4ca51b3b9f99b01a023f2273b5bc37c3317
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-05-14 06:19:31 +0600

    feat: add product component

--- renext-app/page-components/TopPageComponent/TopPageComponent.tsx
+++ renext-app/page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,13 @@
 import { useReducer } from "react";
-import { Advantages, HhData, Htag, P, Sort, Tag } from "../../components";
+import {
+  Advantages,
+  HhData,
+  Htag,
+  P,
+  Product,
+  Sort,
+  Tag,
+} from "../../components";
 import { SortEnum } from "../../components/Sort/Sort.props";
 import { TopLevelCategory } from "../../interfaces/page.interface";
 import { sortReducer } from "./sort.reducer";
@@ -33,7 +41,7 @@ export const TopPageComponent = ({
       </div>
       <div>
         {sortedProducts &&
-          sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}
+          sortedProducts.map((p) => <Product key={p._id} product={p} />)}
       </div>
       <div className={styles.hhtitle}>
         <Htag tag="h2">Вакансии - {page.category}</Htag>
