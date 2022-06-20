30d246551964c35822c20b12bff5f3f6403113c5
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-06-20 17:48:54 +0600

    feat: add meta tags to pages

--- renext-app/pages/_app.tsx
+++ renext-app/pages/_app.tsx
@@ -2,18 +2,23 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <Head>
         <title>DrTop - best shmule</title>
-        <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
         />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta property="og:local" content="ru_RU" />
       </Head>
       <Component {...pageProps} />
     </>
