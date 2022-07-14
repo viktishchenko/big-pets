6d5831ae734359d1405cc3906250202941a856bf
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-07-14 19:39:38 +0600

    feat: configure db with .env file

--- on-store/back/db.js
+++ on-store/back/db.js
@@ -1,3 +1,12 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize({});
+module.exports = new Sequelize(
+  process.env.DB_NAME, // Название
+  process.env.DB_USER, // Пользователь
+  process.env.DB_PASSWORD, // Пароль
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
--- on-store/back/index.js
+++ on-store/back/index.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-
+const sequelize = require("./db");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// anfn
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
