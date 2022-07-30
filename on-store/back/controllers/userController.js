df9abcd50de3c2974854aee738e95e4a9f4e5251
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-07-30 13:08:57 +0600

    fix: separate generate jwt func

--- on-store/back/controllers/userController.js
+++ on-store/back/controllers/userController.js
@@ -3,6 +3,12 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { User, Basket } = require("../models/models");
 
+const generateJWT = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24",
+  });
+};
+
 class UserController {
   async registration(req, res, next) {
     const { email, password, role } = req.body;
@@ -19,11 +25,7 @@ class UserController {
     const hashPassword = await bcrypt.hash(password, 5);
     const user = await User.create({ email, role, password: hashPassword });
     const basket = await Basket.create({ userId: user.id });
-    const token = jwt.sign(
-      { id: user.id, email: user.email, role },
-      process.env.SECRET_KEY,
-      { expiresIn: "24" }
-    );
+    const token = generateJWT(user.id, user.email, user.role);
     return res.json(token);
   }
 
